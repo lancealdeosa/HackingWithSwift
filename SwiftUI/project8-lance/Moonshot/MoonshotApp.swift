3620734ec00d00555b62bca608c763e653158fee
Lance Aldeosa <lance.aldeosa@paymaya.com>
2024-01-04 11:20:37 +0800

    Added initial Moonshot project

---
+++ SwiftUI/project8-lance/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Lance Paolo Aldeosa on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
