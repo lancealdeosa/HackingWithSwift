a4370a1d1de18d4a40226644961325e0c8cb9cff
Lance Aldeosa <lance.aldeosa@paymaya.com>
2024-01-19 17:48:08 +0800

    Added initial Cupcake Corner project

---
+++ SwiftUI/project10-lance/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Lance Paolo Aldeosa on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
